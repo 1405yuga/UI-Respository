2c327f20fc37cab52b9d5afbf80331010a473047
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-12 12:23:08 +0530

    onSelectMenu change color function created

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/MainActivity.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/MainActivity.kt
@@ -1,19 +1,43 @@
 package com.example.homehiveclone
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.example.homehiveclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var navController: NavController
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController=navHostFragment.navController
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.apply {
+            compareLayout.setOnClickListener {
+                setColorForSelectedMenu(binding.compareBar, binding.compareIcon, binding.compareText)
+            }
+        }
     }
+
+    private fun setColorForSelectedMenu(bar: ImageView, icon: ImageView, text: TextView) {
+        val menuColor = ContextCompat.getColor(this, R.color.home_bg_blue)
+        bar.visibility = View.VISIBLE
+        text.setTextColor(menuColor)
+        icon.setColorFilter(menuColor, PorterDuff.Mode.SRC_IN)
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }

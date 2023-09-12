8303487fc2fb6e6b986d339330a78a53bce44e9f
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-12 18:17:16 +0530

    change menu color on selection

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/fragments/CompareFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/fragments/CompareFragment.kt
@@ -1,11 +1,12 @@
 package com.example.homehiveclone.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.homehiveclone.R
 import com.example.homehiveclone.databinding.FragmentCompareBinding
 
 private const val TAG = "CompareFragment tag"
@@ -17,9 +18,27 @@ class CompareFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentCompareBinding.inflate(inflater, container, false)
-        Log.d(TAG, "onCreateView called")
+        binding.propertyListText.setTextColor(ContextCompat.getColor(requireContext(), R.color.home_bg_blue))
+        binding.propertyListBar.visibility = View.VISIBLE
+
+        binding.apply {
+            propertyListLayout.setOnClickListener {
+                binding.propertyListText.setTextColor(ContextCompat.getColor(requireContext(), R.color.home_bg_blue))
+                binding.propertyListBar.visibility = View.VISIBLE
+
+                binding.pastResultsText.setTextColor(ContextCompat.getColor(requireContext(), R.color.nav_menu_text))
+                binding.pastResultsBar.visibility = View.INVISIBLE
+            }
+
+            pastResultsLayout.setOnClickListener {
+                binding.pastResultsText.setTextColor(ContextCompat.getColor(requireContext(), R.color.home_bg_blue))
+                binding.pastResultsBar.visibility = View.VISIBLE
+
+                binding.propertyListText.setTextColor(ContextCompat.getColor(requireContext(), R.color.nav_menu_text))
+                binding.propertyListBar.visibility = View.INVISIBLE
+            }
+        }
         return binding.root
     }
 

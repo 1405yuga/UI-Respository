122a9246a06f335b0bd07c891d795eee0d908639
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-12 15:44:44 +0530

    added remaining blankfragments: activity and profile

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/fragments/ProfileFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/fragments/ProfileFragment.kt
@@ -7,14 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.homehiveclone.R
 
-class SavedFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
 }

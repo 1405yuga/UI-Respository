6d97f63bb572ef8b4ec39587c4364a4d8d232318
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-13 12:59:06 +0530

    added title to saved fragment and top navigation menus

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/fragments/SavedFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/fragments/SavedFragment.kt
@@ -4,17 +4,78 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.homehiveclone.R
+import com.example.homehiveclone.databinding.FragmentSavedBinding
 
 class SavedFragment : Fragment() {
+
+    private lateinit var binding: FragmentSavedBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved, container, false)
+        binding = FragmentSavedBinding.inflate(inflater,container,false)
+        binding.savedPropertyText.setTextColor(
+            ContextCompat.getColor(
+                requireContext(),
+                R.color.home_bg_blue
+            )
+        )
+        binding.savedPropertyBar.visibility = View.VISIBLE
+        loadFragment(ComparePropertyListFragment())
+
+        binding.apply {
+            savedPropertyLayout.setOnClickListener {
+                // TODO: load correct fragment
+                loadFragment(ComparePropertyListFragment())
+                binding.savedPropertyText.setTextColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.home_bg_blue
+                    )
+                )
+                binding.savedPropertyBar.visibility = View.VISIBLE
+
+                binding.savedNotesText.setTextColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.nav_menu_text
+                    )
+                )
+                binding.savedNotesBar.visibility = View.INVISIBLE
+            }
+
+            savedNotesLayout.setOnClickListener {
+                // TODO: load correct fragment
+                loadFragment(ComparePastResultsFragment())
+                binding.savedNotesText.setTextColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.home_bg_blue
+                    )
+                )
+                binding.savedNotesBar.visibility = View.VISIBLE
+
+                binding.savedPropertyText.setTextColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.nav_menu_text
+                    )
+                )
+                binding.savedPropertyBar.visibility = View.INVISIBLE
+            }
+        }
+        return binding.root
+    }
+    private fun loadFragment(fragment: Fragment) {
+        childFragmentManager.beginTransaction()
+            .replace(binding.compareFragmentContainer.id, fragment).setTransition(
+                FragmentTransaction.TRANSIT_FRAGMENT_FADE
+            ).commit()
     }
 
 }

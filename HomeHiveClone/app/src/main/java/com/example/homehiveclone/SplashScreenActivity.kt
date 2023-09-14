52a48cb8f0852cfab4b238ffa733339f2ecf6e8b
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-14 12:56:42 +0530

    attached splash screen

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/SplashScreenActivity.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/SplashScreenActivity.kt
@@ -2,10 +2,21 @@ package com.example.homehiveclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.homehiveclone.splash_fragments.SplashFirstFragment
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        loadFrgament(SplashFirstFragment())
+    }
+
+    private fun loadFrgament(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.splashFragmentContainer,fragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
+            .commit()
     }
 }
--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/splash_fragments/SplashFirstFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/splash_fragments/SplashFirstFragment.kt
@@ -1,10 +1,14 @@
 package com.example.homehiveclone.splash_fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.homehiveclone.MainActivity
 import com.example.homehiveclone.R
 
 class SplashFirstFragment : Fragment() {
@@ -14,7 +18,15 @@ class SplashFirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            loadNextScreen()
+        },1500)
         return inflater.inflate(R.layout.fragment_splash_first, container, false)
+    }
+
+    private fun loadNextScreen() {
+        startActivity(Intent(requireContext(),MainActivity::class.java))
     }
 
 }

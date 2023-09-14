ff0df440758c902f83c56b6c70bb0673a6d57200
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-14 21:11:00 +0530

    attached listing fragment

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/fragments/ProfileFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/fragments/ProfileFragment.kt
@@ -5,16 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.homehiveclone.R
+import com.example.homehiveclone.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater,container,false)
+
+        binding.myListingsButton.setOnClickListener {
+            loadFragment(ProfileListingFragment())
+        }
+        return binding.root
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment, fragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            .addToBackStack(null)
+            .commit()
     }
 
 }
--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/fragments/ProfileListingFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/fragments/ProfileListingFragment.kt
@@ -9,7 +9,6 @@ import com.example.homehiveclone.R
 
 class ProfileListingFragment : Fragment() {
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?

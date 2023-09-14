c15c03b54c771e9cac78c66bc56f321a8087db70
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-14 23:12:26 +0530

    attached first listing fragment

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/ProfileListingActivity.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/ProfileListingActivity.kt
@@ -2,10 +2,25 @@ package com.example.homehiveclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.homehiveclone.databinding.ActivityProfileListingBinding
+import com.example.homehiveclone.mylisting_fragments.ProfileListingPage1Fragment
 
 class ProfileListingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileListingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_listing)
+        binding = ActivityProfileListingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loadFragment(ProfileListingPage1Fragment())
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.listingFragmentContainer.id,fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/fragments/ProfileListingFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/fragments/ProfileListingFragment.kt
@@ -1,20 +1,27 @@
 package com.example.homehiveclone.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.homehiveclone.ProfileListingActivity
 import com.example.homehiveclone.R
+import com.example.homehiveclone.databinding.FragmentProfileListingBinding
 
 class ProfileListingFragment : Fragment() {
 
+    private lateinit var binding : FragmentProfileListingBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_listing, container, false)
+        binding = FragmentProfileListingBinding.inflate(inflater,container,false)
+        binding.listNowButton.setOnClickListener {
+            startActivity(Intent(requireActivity(),ProfileListingActivity::class.java))
+        }
+        return binding.root
     }
 
 }

c4feecd59d08a4e6bff85481a809009d6178e80c
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-14 16:25:30 +0530

    navigate from splash screens to main app

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/splash_fragments/OnboardingThirdFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/splash_fragments/OnboardingThirdFragment.kt
@@ -1,21 +1,28 @@
 package com.example.homehiveclone.splash_fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.homehiveclone.MainActivity
 import com.example.homehiveclone.R
+import com.example.homehiveclone.databinding.FragmentOnboardingThirdBinding
 
 class OnboardingThirdFragment : Fragment() {
 
-
+    private lateinit var binding: FragmentOnboardingThirdBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboarding_third, container, false)
+        binding = FragmentOnboardingThirdBinding.inflate(inflater,container,false)
+        binding.nextButton.setOnClickListener {
+            startActivity(Intent(requireActivity(),MainActivity::class.java))
+            requireActivity().finish()
+        }
+        return binding.root
     }
 
 }

30810a1891eca9436d78e4277cad221eaafcc238
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-16 13:19:29 +0530

    attached splash 2nd screen

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/mylisting_fragments/ProfileListingPage5Fragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/mylisting_fragments/ProfileListingPage5Fragment.kt
@@ -1,11 +1,13 @@
 package com.example.homehiveclone.mylisting_fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.homehiveclone.R
+import com.example.homehiveclone.SplashScreenActivity2
 import com.example.homehiveclone.databinding.FragmentProfileListingPage5Binding
 
 class ProfileListingPage5Fragment : Fragment() {
@@ -15,8 +17,12 @@ class ProfileListingPage5Fragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_listing_page5, container, false)
+        binding = FragmentProfileListingPage5Binding.inflate(inflater,container,false)
+        binding.saveButton.setOnClickListener {
+            startActivity(Intent(requireActivity(),SplashScreenActivity2::class.java))
+            requireActivity().finish()
+        }
+        return binding.root
     }
 
 }

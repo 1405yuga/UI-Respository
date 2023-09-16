464cc6f4af3b10dfc34dcde7e1d8085e263c7923
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-16 15:45:31 +0530

    attached main activity to splash screen

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/SplashScreenActivity2.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/SplashScreenActivity2.kt
@@ -1,11 +1,23 @@
 package com.example.homehiveclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.homehiveclone.databinding.ActivitySplashScreen2Binding
 
 class SplashScreenActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreen2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen2)
+        binding = ActivitySplashScreen2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //splash screen
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+                                                    finish()},2000)
     }
 }

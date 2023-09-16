3c7248d5dc00e206c8390c4892789b6b6ce9b8c0
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-16 16:33:50 +0530

    arranging layout visibility in listing fragment

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/fragments/ProfileListingFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/fragments/ProfileListingFragment.kt
@@ -2,25 +2,79 @@ package com.example.homehiveclone.fragments
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.example.homehiveclone.ProfileListingActivity
 import com.example.homehiveclone.R
 import com.example.homehiveclone.databinding.FragmentProfileListingBinding
 
 class ProfileListingFragment : Fragment() {
 
-    private lateinit var binding : FragmentProfileListingBinding
+    private lateinit var binding: FragmentProfileListingBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentProfileListingBinding.inflate(inflater,container,false)
+        binding = FragmentProfileListingBinding.inflate(inflater, container, false)
+
         binding.listNowButton.setOnClickListener {
-            startActivity(Intent(requireActivity(),ProfileListingActivity::class.java))
+            startActivity(Intent(requireActivity(), ProfileListingActivity::class.java))
+            Handler(Looper.getMainLooper()).postDelayed({
+                binding.noListing.visibility = View.GONE
+                binding.listingAdded.visibility = View.VISIBLE
+            },500)
         }
+
+        binding.yourListingText.setTextColor(
+            ContextCompat.getColor(
+                requireContext(),
+                R.color.home_bg_blue
+            )
+        )
+        binding.yourListingBar.visibility = View.VISIBLE
+        binding.apply {
+            yourListingLayout.setOnClickListener {
+                binding.yourListingText.setTextColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.home_bg_blue
+                    )
+                )
+                binding.yourListingBar.visibility = View.VISIBLE
+
+                binding.analyticsText.setTextColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.nav_menu_text
+                    )
+                )
+                binding.analyticsBar.visibility = View.INVISIBLE
+            }
+
+            analyticsLayout.setOnClickListener {
+                binding.analyticsText.setTextColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.home_bg_blue
+                    )
+                )
+                binding.analyticsBar.visibility = View.VISIBLE
+
+                binding.yourListingText.setTextColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.nav_menu_text
+                    )
+                )
+                binding.yourListingBar.visibility = View.INVISIBLE
+            }
+        }
+
         return binding.root
     }
 

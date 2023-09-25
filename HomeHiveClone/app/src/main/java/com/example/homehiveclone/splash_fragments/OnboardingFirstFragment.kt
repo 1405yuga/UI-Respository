287de08cc3d376fe13cd2f925f5b696e87748d88
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-25 14:19:16 +0530

    removed reductant fragments

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/splash_fragments/OnboardingFirstFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/splash_fragments/OnboardingFirstFragment.kt
@@ -1,17 +1,20 @@
 package com.example.homehiveclone.splash_fragments
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
+import com.example.homehiveclone.MainActivity
 import com.example.homehiveclone.R
 import com.example.homehiveclone.databinding.FragmentOnboardingFirstBinding
 
 class OnboardingFirstFragment : Fragment() {
 
     private lateinit var binding: FragmentOnboardingFirstBinding
+    private var clickCount = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,15 +22,27 @@ class OnboardingFirstFragment : Fragment() {
         binding = FragmentOnboardingFirstBinding.inflate(inflater, container, false)
 
         binding.nextButton.setOnClickListener {
-            loadFragment(OnboardingSecondFragment())
+            clickCount+=1
+            when(clickCount){
+                1 -> setContent(R.drawable.group_1897oboarding2,"Save your time","Compare different properties and get insightful details.")
+                2 -> setContent(R.drawable.group_1913onboarding3,"Get end to end help","Compare different properties and get insightful details.")
+                else -> loadActvity()
+            }
         }
         return binding.root
     }
 
-    private fun loadFragment(fragment: Fragment) {
-        requireActivity().supportFragmentManager.beginTransaction().replace(R.id.splashFragmentContainer,fragment)
-            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
-            .commit()
+    private fun setContent(image: Int, title: String, subTitle: String){
+        binding.apply {
+            // TODO: set image 
+            onBoardingTitle.text = title
+            onBoardingSubtitle.text = subTitle
+        }
+    }
+
+    private fun loadActvity(){
+        startActivity(Intent(requireActivity(), MainActivity::class.java))
+        requireActivity().finish()
     }
 
 }

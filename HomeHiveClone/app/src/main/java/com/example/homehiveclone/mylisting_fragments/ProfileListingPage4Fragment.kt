8b2c293c6ab917c10aae328f3a838c61f994dea8
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-27 21:33:54 +0530

    created function to set allowed and not allowed properties

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/mylisting_fragments/ProfileListingPage4Fragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/mylisting_fragments/ProfileListingPage4Fragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.homehiveclone.R
 import com.example.homehiveclone.databinding.FragmentProfileListingPage2Binding
 import com.example.homehiveclone.databinding.FragmentProfileListingPage4Binding
@@ -23,7 +25,27 @@ class ProfileListingPage4Fragment : Fragment() {
                 .addToBackStack(null)
                 .commit()
         }
+        binding.apply {
+            smokingAllowed.setOnClickListener {
+                changeToSelected(smokingAllowed)
+                changeToUnSelected(smokingNotAllowed)
+            }
+            smokingNotAllowed.setOnClickListener {
+                changeToSelected(smokingNotAllowed)
+                changeToUnSelected(smokingAllowed)
+            }
+        }
         return binding.root
+    }
+
+    private fun changeToUnSelected(textView: TextView) {
+        textView.setTextColor(ContextCompat.getColor(requireContext(),R.color.fragment_top_bar_text))
+        textView.background = ContextCompat.getDrawable(requireContext(),R.drawable.unselected_pet_button)
+    }
+
+    private fun changeToSelected(textView: TextView) {
+        textView.setTextColor(ContextCompat.getColor(requireContext(),R.color.white))
+        textView.background = ContextCompat.getDrawable(requireContext(),R.drawable.selected_pet_button)
     }
 
 }

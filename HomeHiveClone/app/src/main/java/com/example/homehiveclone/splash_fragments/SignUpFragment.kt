f9da7e8edccbb8bc4e30293aeccf80a2cd0117f2
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-27 12:53:01 +0530

    changed getOtp button programatically

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/splash_fragments/SignUpFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/splash_fragments/SignUpFragment.kt
@@ -1,20 +1,48 @@
 package com.example.homehiveclone.splash_fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
 import com.example.homehiveclone.R
+import com.example.homehiveclone.databinding.FragmentSignUpBinding
+
+private const val TAG = "SignUpFragment tag"
 
 class SignUpFragment : Fragment() {
 
+    private lateinit var binding: FragmentSignUpBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+
+        binding.phoneEditText.addTextChangedListener{
+            if(it?.length == 10){
+                binding.getOTPButton.setCardBackgroundColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.activated_blue
+                    )
+                )
+            }
+            else{
+                binding.getOTPButton.setCardBackgroundColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.de_activated_blue
+                    )
+                )
+            }
+        }
+        return binding.root
     }
 
 }

7548f61314ee7abe610011248eb6da1196d0e60a
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-27 14:33:56 +0530

    set focus of otp edit text

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/splash_fragments/VerifyNumberFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/splash_fragments/VerifyNumberFragment.kt
@@ -1,13 +1,18 @@
 package com.example.homehiveclone.splash_fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.homehiveclone.R
 import com.example.homehiveclone.databinding.FragmentVerifyNumberBinding
+
+private val TAG = "VerifyNumberFragment tag"
 
 class VerifyNumberFragment : Fragment() {
 
@@ -16,11 +21,51 @@ class VerifyNumberFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentVerifyNumberBinding.inflate(inflater,container,false)
+        binding = FragmentVerifyNumberBinding.inflate(inflater, container, false)
 
         binding.apply {
             editNumber.setOnClickListener {
                 loadFragment(SignUpFragment())
+            }
+
+            otpDigit1EditText.addTextChangedListener {
+                if (it?.length == 1) {
+                    otpDigit2EditText.requestFocus()
+                }
+            }
+            otpDigit2EditText.addTextChangedListener {
+                if (it?.length == 1) {
+                    otpDigit3EditText.requestFocus()
+                }
+            }
+            otpDigit3EditText.addTextChangedListener {
+                if (it?.length == 1) {
+                    otpDigit4EditText.requestFocus()
+                }
+            }
+            otpDigit4EditText.addTextChangedListener {
+                if (it?.length == 1) {
+                    binding.verifyOTPButton.setCardBackgroundColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.activated_blue
+                        )
+                    )
+                } else {
+                    binding.verifyOTPButton.setCardBackgroundColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.de_activated_blue
+                        )
+                    )
+                }
+            }
+
+            verifyOTPButton.setOnClickListener {
+                if (otpDigit1EditText.length() == 1 && otpDigit2EditText.length() == 1 && otpDigit3EditText.length() == 1 && otpDigit4EditText.length() == 1) {
+                    // TODO: load splash fragment
+                    Log.d(TAG, "Load next screen")
+                }
             }
         }
         return binding.root
@@ -28,7 +73,8 @@ class VerifyNumberFragment : Fragment() {
 
 
     private fun loadFragment(fragment: Fragment) {
-        requireActivity().supportFragmentManager.beginTransaction().replace(R.id.splashFragmentContainer,fragment)
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.splashFragmentContainer, fragment)
             .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
             .commit()
     }

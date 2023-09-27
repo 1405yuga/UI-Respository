29ce15428145988f7da959bb501fa6830de3049a
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-27 20:26:00 +0530

    editText done for maintenance

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/mylisting_fragments/ProfileListingPage2Fragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/mylisting_fragments/ProfileListingPage2Fragment.kt
@@ -1,10 +1,15 @@
 package com.example.homehiveclone.mylisting_fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.ImageView
 import com.example.homehiveclone.R
 import com.example.homehiveclone.databinding.FragmentProfileListingPage2Binding
 import com.example.homehiveclone.databinding.FragmentProfileListingPage3Binding
@@ -12,6 +17,8 @@ import com.example.homehiveclone.databinding.FragmentProfileListingPage3Binding
 class ProfileListingPage2Fragment : Fragment() {
 
     private lateinit var binding: FragmentProfileListingPage2Binding
+    private val maintenanceTypes = arrayOf("Included","Excluded")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,7 +30,26 @@ class ProfileListingPage2Fragment : Fragment() {
                 .addToBackStack(null)
                 .commit()
         }
+
+        binding.apply {
+            setEditTextAdapters(maintenanceEditText,maintenanceTypes,true)
+        }
         return binding.root
+    }
+    private fun setEditTextAdapters(
+        editText: AutoCompleteTextView,
+        dropDownArray: Array<String>,
+        showEntireList: Boolean
+    ) {
+        editText.setAdapter(ArrayAdapter(requireContext(), androidx.transition.R.layout.support_simple_spinner_dropdown_item,dropDownArray))
+        editText.threshold = 1
+        if(showEntireList){
+            editText.setOnTouchListener { view, motionEvent ->
+                editText.showDropDown()
+                false
+            }
+        }
+
     }
 
 }

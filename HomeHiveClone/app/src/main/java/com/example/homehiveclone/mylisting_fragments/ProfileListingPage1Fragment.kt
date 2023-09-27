8c833f9d15a1173181ba9f782ab4e0084f776c0d
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-27 15:59:03 +0530

    created function to complete edit text by auto text view

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/mylisting_fragments/ProfileListingPage1Fragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/mylisting_fragments/ProfileListingPage1Fragment.kt
@@ -1,16 +1,25 @@
 package com.example.homehiveclone.mylisting_fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.ImageView
 import com.example.homehiveclone.R
 import com.example.homehiveclone.databinding.FragmentProfileListingPage1Binding
+
+private const val TAG = "ProfileListingPage1Fragment tag"
 
 class ProfileListingPage1Fragment : Fragment() {
 
     private lateinit var binding: FragmentProfileListingPage1Binding
+    private val cityNames = arrayOf("Mumbai City","Mumbai Suburban","Delhi","Kolkata","Punjab")
+    private val localityNames = arrayOf("Andheri","Bandra","Borivali","Goregaon")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,7 +31,26 @@ class ProfileListingPage1Fragment : Fragment() {
                 .addToBackStack(null)
                 .commit()
         }
+
+        binding.apply {
+            setEditTextAdapters(cityNameEditText,cityNames,cityNameDropDown)
+            setEditTextAdapters(localityEditText,localityNames,localityDropDown)
+        }
         return binding.root
+    }
+
+    private fun setEditTextAdapters(
+        editText: AutoCompleteTextView,
+        dropDownArray: Array<String>,
+        dropDownButton: ImageView
+    ) {
+        editText.setAdapter(ArrayAdapter(requireContext(), androidx.transition.R.layout.support_simple_spinner_dropdown_item,dropDownArray))
+        editText.threshold = 1
+        editText.setOnItemClickListener { adapterView, view, i, l ->
+            val temp = adapterView.getItemAtPosition(i).toString()
+            Log.d(TAG , "Selected text : $temp")
+            dropDownButton.visibility = View.GONE
+        }
     }
 
 }

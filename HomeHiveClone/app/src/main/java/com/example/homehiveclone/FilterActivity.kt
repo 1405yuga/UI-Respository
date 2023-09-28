4822c4d1c99433d4f2de31893a82d6226f8327b3
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-28 14:48:31 +0530

    attached city filter fragment

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/FilterActivity.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/FilterActivity.kt
@@ -2,10 +2,23 @@ package com.example.homehiveclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.homehiveclone.databinding.ActivityFilterBinding
+import com.example.homehiveclone.filter_fragments.CityFilterFragment
 
 class FilterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFilterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_filter)
+        binding= ActivityFilterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loadFragment(CityFilterFragment())
     }
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.filterFragmentContainer.id, fragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            .commit()}
 }

f5d9d6c512690e7c929e80b06b007d98248f5647
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-28 15:46:59 +0530

    set on click for next button

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/filter_fragments/CityFilterFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/filter_fragments/CityFilterFragment.kt
@@ -1,19 +1,31 @@
 package com.example.homehiveclone.filter_fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.homehiveclone.R
+import com.example.homehiveclone.databinding.FragmentCityFilterBinding
 
 class CityFilterFragment : Fragment() {
 
+    private lateinit var binding: FragmentCityFilterBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_city_filter, container, false)
+        binding = FragmentCityFilterBinding.inflate(inflater, container, false)
+        binding.nextButton.setOnClickListener {
+            loadFragment(PropertyFilterFragment())
+        }
+        return binding.root
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.filterFragmentContainer, fragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit()
     }
 }

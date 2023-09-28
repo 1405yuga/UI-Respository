044f4a263e99eb73ae256ba387afbc30eeaa148d
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-28 12:57:04 +0530

    added background for deactivated - activated button

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/mylisting_fragments/ProfileListingYourListingFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/mylisting_fragments/ProfileListingYourListingFragment.kt
@@ -1,20 +1,32 @@
 package com.example.homehiveclone.mylisting_fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.example.homehiveclone.R
+import com.example.homehiveclone.databinding.FragmentProfileListingYourListingBinding
 
 class ProfileListingYourListingFragment : Fragment() {
 
+    private lateinit var binding: FragmentProfileListingYourListingBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_listing_your_listing, container, false)
+        binding = FragmentProfileListingYourListingBinding.inflate(inflater, container, false)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.deactiveTextView.background =
+                ContextCompat.getDrawable(requireContext(),R.drawable.connect_button)
+
+        }, 5000)
+
+        return binding.root
     }
 
 }

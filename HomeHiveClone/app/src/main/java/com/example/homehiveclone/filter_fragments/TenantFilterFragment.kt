39d01832f0011334e0322131a5ff5bdd4964c56a
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-28 18:26:44 +0530

    added gender

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/filter_fragments/TenantFilterFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/filter_fragments/TenantFilterFragment.kt
@@ -5,16 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.homehiveclone.R
+import com.example.homehiveclone.databinding.FragmentTenantFilterBinding
 
 class TenantFilterFragment : Fragment() {
 
+    private lateinit var binding: FragmentTenantFilterBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tenant_filter, container, false)
+        binding = FragmentTenantFilterBinding.inflate(inflater,container,false)
+        binding.apply {
+            //gender
+            onlyBoys.setOnClickListener { clickedProperty(onlyBoys) }
+            onlyGirls.setOnClickListener { clickedProperty(onlyGirls) }
+            anyGender.setOnClickListener { clickedProperty(anyGender) }
+        }
+        return binding.root
     }
 
+    private fun clickedProperty(textView: TextView) {
+        textView.tag = !textView.tag.toString().toBoolean()
+        if (textView.tag as Boolean) {
+            textView.background =
+                ContextCompat.getDrawable(requireContext(), R.drawable.selected_filter)
+            textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+        } else {
+            textView.background =
+                ContextCompat.getDrawable(requireContext(), R.drawable.unselected_filter)
+            textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.contaced_text))
+        }
+    }
 }

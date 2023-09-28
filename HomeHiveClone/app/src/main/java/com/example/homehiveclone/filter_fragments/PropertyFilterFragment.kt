4a5cf8c460df0b229232bdeac03b06d4599525cd
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-28 16:36:59 +0530

    loaded child fragments in property filter fragment

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/filter_fragments/PropertyFilterFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/filter_fragments/PropertyFilterFragment.kt
@@ -1,21 +1,37 @@
 package com.example.homehiveclone.filter_fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.homehiveclone.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.homehiveclone.databinding.FragmentPropertyFilterBinding
 
 class PropertyFilterFragment : Fragment() {
 
-
+    private lateinit var binding: FragmentPropertyFilterBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_property_filter, container, false)
+        binding = FragmentPropertyFilterBinding.inflate(inflater, container, false)
+        loadFragment(FlatFilterFragment())
+
+        binding.apply {
+            flatFilterButton.setOnClickListener {
+                loadFragment(FlatFilterFragment())
+            }
+            tenantFilterButton.setOnClickListener { loadFragment(TenantFilterFragment()) }
+        }
+        return binding.root
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        childFragmentManager.beginTransaction()
+            .replace(binding.propertyFilterFragmentContainer.id, fragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            .commit()
     }
 
 }

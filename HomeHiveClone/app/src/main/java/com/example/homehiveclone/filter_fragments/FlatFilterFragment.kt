b878349b22cc7b37b5d7689a605ef83edd551f58
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-29 11:30:45 +0530

    set location edit text

--- HomeHiveClone/app/src/main/java/com/example/homehiveclone/filter_fragments/FlatFilterFragment.kt
+++ HomeHiveClone/app/src/main/java/com/example/homehiveclone/filter_fragments/FlatFilterFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -14,6 +16,8 @@ import com.example.homehiveclone.databinding.FragmentFlatFilterBinding
 class FlatFilterFragment : Fragment() {
 
     private lateinit var binding: FragmentFlatFilterBinding
+    private val localityNames = arrayOf("Andheri","Airoli","Bandra","Borivali","Goregaon")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,6 +25,8 @@ class FlatFilterFragment : Fragment() {
         binding = FragmentFlatFilterBinding.inflate(inflater, container, false)
 
         binding.apply {
+            setEditTextAdapters(localityEditText,localityNames)
+
             // property type
             independentHouse.setOnClickListener { clickedProperty(independentHouse) }
             apartment.setOnClickListener { clickedProperty(apartment) }
@@ -47,6 +53,20 @@ class FlatFilterFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    private fun setEditTextAdapters(
+        editText: AutoCompleteTextView,
+        dropDownArray: Array<String>,
+    ) {
+        editText.setAdapter(
+            ArrayAdapter(
+                requireContext(),
+                androidx.transition.R.layout.support_simple_spinner_dropdown_item,
+                dropDownArray
+            )
+        )
+        editText.threshold = 1
     }
 
     private fun clickedProperty(textView: TextView) {

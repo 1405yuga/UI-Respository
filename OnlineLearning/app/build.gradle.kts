692c145c667672709de27ff6a50def9380a35e9a
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-10-01 00:51:48 +0530

    set onboarding image and next button

--- OnlineLearning/app/build.gradle.kts
+++ OnlineLearning/app/build.gradle.kts
@@ -52,4 +52,7 @@ dependencies {
     val nav_version = "2.5.2"
     implementation ("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation ("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // Coil - to load images
+    implementation ("io.coil-kt:coil:1.1.1")
 }
--- OnlineLearning/app/src/main/java/com/example/onlinelearning/entryFragments/OnBoardingFragment.kt
+++ OnlineLearning/app/src/main/java/com/example/onlinelearning/entryFragments/OnBoardingFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import coil.load
 import com.example.onlinelearning.R
 import com.example.onlinelearning.databinding.FragmentOnBoardingBinding
 
@@ -24,7 +25,10 @@ class OnBoardingFragment : Fragment() {
             click+=1
             when(click){
                 1 -> setNewOnBoarding(R.drawable.illustration_02,"Quick and easy \n learning","Easy and fast learning at \nany time to help you \n improve various skills",R.drawable.pavigation2)
-                2 -> setNewOnBoarding(R.drawable.illustration_03,"Create your own \nstudy plan","Study according to the \nstudy plan, make study \nmore motivated",R.drawable.pavigation3)
+                2 -> {
+                    binding.skipButton.visibility = View.INVISIBLE
+                    setNewOnBoarding(R.drawable.illustration_03,"Create your own \nstudy plan","Study according to the \nstudy plan, make study \nmore motivated",R.drawable.pavigation3)
+                }
                 // TODO: load fragment
             }
         }
@@ -34,10 +38,14 @@ class OnBoardingFragment : Fragment() {
 
     private fun setNewOnBoarding(image: Int, title: String, subtitle: String, nextButton: Int) {
         binding.apply {
-            // TODO: set image
+            onBoardingImage.load(image){
+                placeholder(image)
+            }
             onBoardingTitle.text = title
             onBoardingSubtitle.text = subtitle
-            // TODO: set button
+            nextBar.load(nextButton){
+                placeholder(nextButton)
+            }
         }
 
     }

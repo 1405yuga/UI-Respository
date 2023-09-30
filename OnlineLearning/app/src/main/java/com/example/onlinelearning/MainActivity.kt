6ae3666ac398c6adc28edb49c736fb2d9391bcfd
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-30 15:46:45 +0530

    attached fragment

--- OnlineLearning/app/src/main/java/com/example/onlinelearning/MainActivity.kt
+++ OnlineLearning/app/src/main/java/com/example/onlinelearning/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.onlinelearning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.navHostFragmentConatiner) as NavHostFragment
+        navController = navHost.navController
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }

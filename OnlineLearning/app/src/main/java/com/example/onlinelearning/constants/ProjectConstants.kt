a54a6f17260f7374309cef6d30370399646154b2
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-10-06 13:07:20 +0530

    created menu constants

---
+++ OnlineLearning/app/src/main/java/com/example/onlinelearning/constants/ProjectConstants.kt
@@ -0,0 +1,8 @@
+package com.example.onlinelearning.constants
+
+object ProjectConstants {
+    const val HOME_MENU = "Home"
+    const val COURSE_MENU = "Course"
+    const val MESSAGE_MENU = "Message"
+    const val ACCOUNT_MENU = "Account"
+}

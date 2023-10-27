611162be87d0d3a9d53ed75da467640f12c65e75
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-10-27 19:15:13 +0530

    created messages menu fragments

---
+++ OnlineLearning/app/src/main/java/com/example/onlinelearning/message_fragments/MessageMessageFragment.kt
@@ -0,0 +1,20 @@
+package com.example.onlinelearning.message_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.onlinelearning.R
+
+class MessageMessageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_message_message, container, false)
+    }
+
+}

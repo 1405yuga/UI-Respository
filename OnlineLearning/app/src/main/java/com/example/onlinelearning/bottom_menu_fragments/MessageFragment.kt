a3e4cd2aed37bdf3c8b37ce377ed67cc0559b239
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-10-27 19:04:10 +0530

    added message and notification menu onclick

--- OnlineLearning/app/src/main/java/com/example/onlinelearning/bottom_menu_fragments/MessageFragment.kt
+++ OnlineLearning/app/src/main/java/com/example/onlinelearning/bottom_menu_fragments/MessageFragment.kt
@@ -5,16 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.onlinelearning.R
+import com.example.onlinelearning.databinding.FragmentMessageBinding
 
 class MessageFragment : Fragment() {
 
+    private lateinit var binding: FragmentMessageBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        binding = FragmentMessageBinding.inflate(inflater,container,false)
+
+        binding.apply {
+            messageTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.message_selected_text))
+            notificationTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.message_unselected_text))
+            messageBar.visibility = View.VISIBLE
+            notificationBar.visibility=View.INVISIBLE
+
+            messageLayout.setOnClickListener {
+                messageTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.message_selected_text))
+                notificationTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.message_unselected_text))
+                messageBar.visibility = View.VISIBLE
+                notificationBar.visibility=View.INVISIBLE
+            }
+            notificationLayout.setOnClickListener {
+                notificationTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.message_selected_text))
+                messageTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.message_unselected_text))
+                messageBar.visibility = View.INVISIBLE
+                notificationBar.visibility=View.VISIBLE
+            }
+        }
+
+        return binding.root
     }
 
 }

446ee0d8d2a5c259d8ac73b5c8e7ce76844db40c
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-10-27 14:33:10 +0530

    setonlick for coursetype

--- OnlineLearning/app/src/main/java/com/example/onlinelearning/bottom_menu_fragments/CourseFragment.kt
+++ OnlineLearning/app/src/main/java/com/example/onlinelearning/bottom_menu_fragments/CourseFragment.kt
@@ -5,16 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.onlinelearning.R
+import com.example.onlinelearning.databinding.FragmentCourseBinding
 
 class CourseFragment : Fragment() {
+
+    private lateinit var binding: FragmentCourseBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_course, container, false)
+        binding = FragmentCourseBinding.inflate(inflater,container,false)
+
+
+        binding.apply {
+            setColorForSelectedMenu(allCourseType)
+
+            allCourseType.setOnClickListener {
+                setColorForSelectedMenu(allCourseType)
+                setColorForUnselectedMenu(newCourseType)
+                setColorForUnselectedMenu(popularCourseType)
+            }
+            popularCourseType.setOnClickListener {
+                setColorForSelectedMenu(popularCourseType)
+                setColorForUnselectedMenu(newCourseType)
+                setColorForUnselectedMenu(allCourseType)
+            }
+            newCourseType.setOnClickListener {
+                setColorForSelectedMenu(newCourseType)
+                setColorForUnselectedMenu(popularCourseType)
+                setColorForUnselectedMenu(allCourseType)
+            }
+        }
+        return binding.root
+    }
+
+    private fun setColorForUnselectedMenu(courseType: TextView) {
+        courseType.setTextColor(ContextCompat.getColor(requireContext(),R.color.course_type_unselected_text))
+        courseType.background.setTint(ContextCompat.getColor(requireContext(),R.color.course_type_unselected_bg))
+    }
+
+    private fun setColorForSelectedMenu(courseType: TextView) {
+        courseType.setTextColor(ContextCompat.getColor(requireContext(),R.color.white))
+        courseType.background.setTint(ContextCompat.getColor(requireContext(),R.color.sign_up_button_bg))
     }
 
 }

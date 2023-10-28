63995c530eea7a0a9b8b83ce4b61fe3ae7cdc19b
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-10-28 15:20:21 +0530

    optimize message fragment backend

--- OnlineLearning/app/src/main/java/com/example/onlinelearning/bottom_menu_fragments/MessageFragment.kt
+++ OnlineLearning/app/src/main/java/com/example/onlinelearning/bottom_menu_fragments/MessageFragment.kt
@@ -1,11 +1,12 @@
 package com.example.onlinelearning.bottom_menu_fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.onlinelearning.R
@@ -15,38 +16,49 @@ class MessageFragment : Fragment() {
 
     private lateinit var binding: FragmentMessageBinding
     private lateinit var navController: NavController
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMessageBinding.inflate(inflater,container,false)
-        val navHost = childFragmentManager.findFragmentById(binding.messageFragmentContainer.id) as NavHostFragment
+
+        binding = FragmentMessageBinding.inflate(inflater, container, false)
+        val selectedMenuText =
+            ContextCompat.getColor(requireContext(), R.color.message_selected_text)
+        val unselectedMenuText =
+            ContextCompat.getColor(requireContext(), R.color.message_unselected_text)
+
+        val navHost =
+            childFragmentManager.findFragmentById(binding.messageFragmentContainer.id) as NavHostFragment
         navController = navHost.navController
 
-        binding.apply {
-            messageTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.message_selected_text))
-            notificationTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.message_unselected_text))
-            messageBar.visibility = View.VISIBLE
-            notificationBar.visibility=View.INVISIBLE
-            navController.navigate(R.id.messageMessageFragment)
+        val choice = MutableLiveData(MessageChoices.MESSAGE)
 
-            messageLayout.setOnClickListener {
-                messageTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.message_selected_text))
-                notificationTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.message_unselected_text))
-                messageBar.visibility = View.VISIBLE
-                notificationBar.visibility=View.INVISIBLE
-                navController.navigate(R.id.messageMessageFragment)
-            }
-            notificationLayout.setOnClickListener {
-                notificationTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.message_selected_text))
-                messageTextView.setTextColor(ContextCompat.getColor(requireContext(),R.color.message_unselected_text))
-                messageBar.visibility = View.INVISIBLE
-                notificationBar.visibility=View.VISIBLE
-                navController.navigate(R.id.messageNotificationFragment)
+        choice.observe(viewLifecycleOwner) {
+            binding.apply {
+                messageTextView.setTextColor(if (it == MessageChoices.MESSAGE) selectedMenuText else unselectedMenuText)
+                messageBar.visibility = (if (it == MessageChoices.MESSAGE) View.VISIBLE else View.INVISIBLE)
+
+                notificationTextView.setTextColor(if (it == MessageChoices.NOTIFICATION) selectedMenuText else unselectedMenuText)
+                notificationBar.visibility = (if(it==MessageChoices.NOTIFICATION) View.VISIBLE else View.INVISIBLE)
+
+                navController.navigate(if(it==MessageChoices.MESSAGE) R.id.messageMessageFragment else R.id.messageNotificationFragment)
             }
         }
 
+        binding.apply {
+            messageLayout.setOnClickListener {
+                choice.value = MessageChoices.MESSAGE
+
+            }
+            notificationLayout.setOnClickListener {
+                choice.value = MessageChoices.NOTIFICATION
+            }
+        }
         return binding.root
     }
+}
 
+private enum class MessageChoices {
+    MESSAGE, NOTIFICATION
 }

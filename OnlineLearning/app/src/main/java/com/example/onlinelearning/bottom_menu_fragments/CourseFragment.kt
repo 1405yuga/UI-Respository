a7e6be880f3faf41ffb77b323b91633a3a2590d2
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-10-28 15:04:51 +0530

    optimize course fragment backend

--- OnlineLearning/app/src/main/java/com/example/onlinelearning/bottom_menu_fragments/CourseFragment.kt
+++ OnlineLearning/app/src/main/java/com/example/onlinelearning/bottom_menu_fragments/CourseFragment.kt
@@ -1,12 +1,13 @@
 package com.example.onlinelearning.bottom_menu_fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import com.example.onlinelearning.R
 import com.example.onlinelearning.databinding.FragmentCourseBinding
 
@@ -14,44 +15,58 @@ class CourseFragment : Fragment() {
 
     private lateinit var binding: FragmentCourseBinding
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentCourseBinding.inflate(inflater,container,false)
+        binding = FragmentCourseBinding.inflate(inflater, container, false)
+        val selectedMenuText = ContextCompat.getColor(requireContext(), R.color.white)
+        val unselectedMenuText =
+            ContextCompat.getColor(requireContext(), R.color.course_type_unselected_text)
+        val selectedMenuTint = ContextCompat.getColor(requireContext(), R.color.sign_up_button_bg)
+        val unselectedMenuTint =
+            ContextCompat.getColor(requireContext(), R.color.course_type_unselected_bg)
 
+        val choice = MutableLiveData(CourseTypes.ALL)
+
+        choice.observe(viewLifecycleOwner) {
+            binding.apply {
+                allCourseType.apply {
+                    setTextColor(if (it == CourseTypes.ALL) selectedMenuText else unselectedMenuText)
+                    background.setTint(if (it == CourseTypes.ALL) selectedMenuTint else unselectedMenuTint)
+                }
+
+                popularCourseType.apply {
+                    setTextColor(if (it == CourseTypes.POPULAR) selectedMenuText else unselectedMenuText)
+                    background.setTint(if(it == CourseTypes.POPULAR) selectedMenuTint else unselectedMenuTint)
+                }
+
+                newCourseType.apply {
+                    setTextColor(if (it == CourseTypes.NEW) selectedMenuText else unselectedMenuText)
+                    background.setTint(if(it == CourseTypes.NEW) selectedMenuTint else unselectedMenuTint)
+                }
+
+            }
+        }
 
         binding.apply {
-            setColorForSelectedMenu(allCourseType)
 
             allCourseType.setOnClickListener {
-                setColorForSelectedMenu(allCourseType)
-                setColorForUnselectedMenu(newCourseType)
-                setColorForUnselectedMenu(popularCourseType)
+                choice.value = CourseTypes.ALL
             }
             popularCourseType.setOnClickListener {
-                setColorForSelectedMenu(popularCourseType)
-                setColorForUnselectedMenu(newCourseType)
-                setColorForUnselectedMenu(allCourseType)
+                choice.value = CourseTypes.POPULAR
+
             }
             newCourseType.setOnClickListener {
-                setColorForSelectedMenu(newCourseType)
-                setColorForUnselectedMenu(popularCourseType)
-                setColorForUnselectedMenu(allCourseType)
+                choice.value = CourseTypes.NEW
             }
         }
         return binding.root
     }
 
-    private fun setColorForUnselectedMenu(courseType: TextView) {
-        courseType.setTextColor(ContextCompat.getColor(requireContext(),R.color.course_type_unselected_text))
-        courseType.background.setTint(ContextCompat.getColor(requireContext(),R.color.course_type_unselected_bg))
-    }
+}
 
-    private fun setColorForSelectedMenu(courseType: TextView) {
-        courseType.setTextColor(ContextCompat.getColor(requireContext(),R.color.white))
-        courseType.background.setTint(ContextCompat.getColor(requireContext(),R.color.sign_up_button_bg))
-    }
-
+private enum class CourseTypes {
+    ALL, POPULAR, NEW
 }
